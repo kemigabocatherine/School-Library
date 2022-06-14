fd88cf324209931b8fba04808959ced639f5b677
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-06-15 00:52:33 +0300

    Add person class and file

---
+++ person.rb
@@ -0,0 +1,25 @@
+require 'securerandom'
+
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = SecureRandom.uuid
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || parent_permission
+  end
+end
