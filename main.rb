ac9ecccf313b5c12aa3d64a18dfa1348d2769350
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-06-18 14:47:58 +0300

    Add methods for creating a person, a student, a teacher and a rental

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
 require './storage'
+require './book'
+require './person'
+require './teacher'
+require './student'
+require './rental'
 
 class App
   attr_reader :storage
@@ -11,7 +16,7 @@ class App
     puts 'Welcome to School Library App!!'
     puts ''
 
-    puts 'Please choose an option by entering a number:'
+    puts 'Please choose an option by entering a number: '
     puts '1-List all books.'
     puts '2-List all people.'
     puts '3-Create a person.' 
@@ -42,6 +47,62 @@ class App
     @storage.books.each do |book|
       puts 'Title: \"#{book.title}\", Author: #{book.author}'
     end
+    base_menu
+  end
+
+  def list_all_people
+    @storage.people.each do |person|
+      puts '[#{person.class}]Name: #{person.name}, ID: #{person.id} Age: #{person.age}'
+    end
+    base_menu
+  end
+
+  def create_a_person
+    puts 'Do you want to create a student (1) or a teacher (2) [Input the number]: '
+    menu_option = gets.chomp
+
+    case menu_option
+    when '1' then create_a_student
+    when '2' then create_a_teacher
+    else  
+      puts 'Invalid request'
+      create_a_person
+    end
+    base_menu
+  end
+
+  def create_a_student
+    puts 'Age: '
+    age = gets.chomp
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Has parent permission? (Y/N): '
+    parent_permission = gets.chomp.downcase == 'y'
+    student = Student.new(age, 'n/a', name, parent_permission: parent_permission)
+    @storage.people << student
+    puts 'Person created successfully'
+  end
+
+  def create_a_teacher
+    puts 'Age: '
+    age = gets.chomp
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @storage.people << teacher
+    puts 'Person created successfully'
+  end
+
+  def create_a_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    @storage.books << book
+    puts 'Book created successfully'
     base_menu
   end
 end
